1cbeb5d4b507ea3b0ce2e7bd769d045e5941a7e6
Vlad Stoenescu <64587367+vladstoe@users.noreply.github.com>
2023-07-23 01:04:38 +0300

    logreg works

--- LoginRegisterPage.js
+++ LoginRegisterPage.js
@@ -7,43 +7,117 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
+  Alert,
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const LoginRegisterPage = () => {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [isLogin, setIsLogin] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleLoginRegister = () => {
-    // Add your login/register logic here
     if (isLogin) {
-      // Handle login
-      console.log('Login:', email, password);
+      // Simulate login - Add your actual login logic here
+      if (email === '' || password === '') {
+        Alert.alert('Error', 'Please enter your email and password.');
+      } else {
+        console.log('Login:', email, password);
+      }
     } else {
-      // Handle register
-      console.log('Register:', email, password);
+      // Simulate registration - Add your actual registration logic here
+      if (username === '' || email === '' || password === '' || confirmPassword === '') {
+        Alert.alert('Error', 'Please fill in all fields.');
+      } else if (!isValidEmail(email)) {
+        Alert.alert('Error', 'Please enter a valid email address.');
+      } else if (password !== confirmPassword) {
+        Alert.alert('Error', 'Passwords do not match.');
+      } else {
+        console.log('Registration successful:', username, email, password);
+      }
     }
+  };
+
+  const isValidEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(prevState => !prevState);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(prevState => !prevState);
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{isLogin ? 'Login' : 'Register'}</Text>
+      {!isLogin && (
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={text => setUsername(text)}
+          value={username}
+          placeholderTextColor="#aaa"
+        />
+      )}
       <TextInput
         style={styles.input}
         placeholder="Email"
         onChangeText={text => setEmail(text)}
         value={email}
         keyboardType="email-address"
+        autoCapitalize="none"
         placeholderTextColor="#aaa"
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={text => setPassword(text)}
-        value={password}
-        secureTextEntry
-        placeholderTextColor="#aaa"
-      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.passwordInput}
+          placeholder="Password"
+          onChangeText={text => setPassword(text)}
+          value={password}
+          secureTextEntry={!showPassword}
+          placeholderTextColor="#aaa"
+        />
+        <TouchableOpacity
+          style={styles.passwordVisibilityButton}
+          onPress={togglePasswordVisibility}
+        >
+          <Ionicons
+            name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+            size={24}
+            color="#007bff"
+          />
+        </TouchableOpacity>
+      </View>
+      {!isLogin && (
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            placeholder="Confirm Password"
+            onChangeText={text => setConfirmPassword(text)}
+            value={confirmPassword}
+            secureTextEntry={!showConfirmPassword}
+            placeholderTextColor="#aaa"
+          />
+          <TouchableOpacity
+            style={styles.passwordVisibilityButton}
+            onPress={toggleConfirmPasswordVisibility}
+          >
+            <Ionicons
+              name={showConfirmPassword ? 'eye-off-outline' : 'eye-outline'}
+              size={24}
+              color="#007bff"
+            />
+          </TouchableOpacity>
+        </View>
+      )}
       <TouchableOpacity
         style={styles.button}
         onPress={handleLoginRegister}
@@ -84,9 +158,31 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     marginBottom: 16,
+    paddingHorizontal: 16, // Adjusted back to 16 for username and email fields
+    fontSize: 16,
+    color: '#333',
+  },
+  passwordContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  passwordInput: {
+    flex: 1,
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 16,
     paddingHorizontal: 16,
     fontSize: 16,
     color: '#333',
+  },
+  passwordVisibilityButton: {
+    position: 'absolute',
+    right: 16,
+    top: 12,
   },
   button: {
     backgroundColor: '#007bff',
