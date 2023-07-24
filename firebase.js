839ab6adffd7e75c94dfc4068668491e213ea5c3
Vlad Stoenescu <64587367+vladstoe@users.noreply.github.com>
2023-07-24 18:27:44 +0300

    firebase

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDzJRE6n3h78HCZ7TFDb7b_pVf7aLgvTJY",
+    authDomain: "bani-native.firebaseapp.com",
+    projectId: "bani-native",
+    storageBucket: "bani-native.appspot.com",
+    messagingSenderId: "202438382912",
+    appId: "1:202438382912:web:080c8b00ee6152fe3bd6a5",
+    measurementId: "G-E8FJTCRTW0"
+  };
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
